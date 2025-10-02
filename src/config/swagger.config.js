c13f30aaf495483582c1b7ee99945d48876eea20
Sridesh Fernando <srideshhimalka@gmail.com>
2025-10-02 23:51:05 +0530

    swagger docs setup

---
+++ src/config/swagger.config.js
@@ -0,0 +1,23 @@
+const swaggerJsDoc = require("swagger-jsdoc");
+
+const options = {
+  definition: {
+    openapi: "3.0.0",
+    info: {
+      title: "Real Time Bus Tracking System - API",
+      version: "1.0.0",
+      description:
+        "A web API service to track, manage and update buses and routes",
+    },
+    servers: [
+      {
+        url: "http//localhost:3000/api/v1",
+      },
+    ],
+  },
+  apis: ["./src/routes/*.js"],
+};
+
+const swaggerSpecs = swaggerJsDoc(options);
+
+module.exports = swaggerSpecs;
