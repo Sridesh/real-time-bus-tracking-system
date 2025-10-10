7deccc076cc2d841504438463bc052f6f6944a91
Sridesh Fernando <srideshhimalka@gmail.com>
2025-10-11 04:15:18 +0530

    location routes added

--- src/routes/index.js
+++ src/routes/index.js
@@ -1,10 +1,12 @@
 const express = require('express');
 const busesRoutes = require('./v1/bus.routes');
 const authRoutes = require('./v1/auth.routes');
+const locationRoutes = require('./v1/location.route');
 
 const router = express.Router();
 
 router.use('/v1/buses', busesRoutes);
 router.use('/v1/auth', authRoutes);
+router.use('/v1/location', locationRoutes);
 
 module.exports = router;
